e9be24fdf43f963d4565fca14654d7c631f26822
joony0219 <70154828+joony0219@users.noreply.github.com>
2023-05-23 14:39:43 +0900

    first commit

---
+++ pages/create.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+import Layout from "@/components/Layout"
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>WEB!</title>
+      </Head>
+      <h2>Create!</h2>
+      Hello, Create
+    </>
+  )
+}
